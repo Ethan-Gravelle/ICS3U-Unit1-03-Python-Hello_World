45b5a372180a72bed9e252a5344e787d69149424
Ubuntu <ubuntu@ip-172-31-1-217.ec2.internal>
2021-04-23 14:17:39 +0000

    added in hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by Ethan Gravelle
+# Created April 2021
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello,World!")
+
+
+if __name__ == "__main__":
+    main()
+    
